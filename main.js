0c110bae3a242db979a10c5ebcf2663dda07ca23
Carlos L Molina <carlos.molina@codeimmersives.com>
2020-06-11 15:39:19 -0400

    changes

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 /********************
  * YOUR CODE BELOW! *
  ********************/
-
+function moreThan5(num1){
+  return num1 > 5;
+  }
+  function topScore(num1, num2) {
+  return num1 >num2;
+  }
+  function isInDanger(grade) {
+  return 60 <= grade && grade <= 71;
+  }
+  function isCoasting(grade) {
+  return 72 <= grade && grade <= 83;
+  }
+  function isSucceeding(grade) {
+  return 84 <= grade && grade <= 92;
+  }
+  function isFailing(num) {
+  return num< 60; 
+  }
+  function isAcing(grade) {
+  return grade > 92 ; 
+  }
+  function isStudent(role){
+  return role === 'student' ;
+  }
+  function isTeacher(role) {
+  return role === 'teacher' ;
+  }
+  function isAdmin(role) {
+  return role === 'admin' ;
+  }
+  function isElementary(level) {
+  return level === 'elementary' ;
+  }
+  function isMiddleSchoolTeacher(role, level) {
+  return role === 'teacher' && (level >= 6 &&
+    level <= 8);
+  }
+  function notAnElementarySchoolAdministrator(level, role) {
+  return level !== 'elementary' || role !== 'admin' ;
+  }
+  function areDifferentPeople(name1, name2) {
+  return name1 !== name2;
+  }
 
 
 
